642b0cd93e106faa0f02f2e65babc7a3bf0bef91
Ankit Saxena <saxenankit@gmail.com>
2026-09-25 20:42:06 +0530

    corrected label for select org

--- src/views/ProfileCompletion.vue
+++ src/views/ProfileCompletion.vue
@@ -163,7 +163,7 @@ onMounted(() => {
           class="w-full bg-white border border-gray-200 rounded-2xl px-4 py-2 text-gray-600 appearance-none focus:outline-none focus:ring-2 focus:ring-primary-500 shadow-sm font-medium"
         >
           <option value="" disabled>
-            {{ t('addPost.selectCategory') }}
+            {{ t('profileCompletion.selectOrganisationLabel') }}
           </option>
 
           <option
